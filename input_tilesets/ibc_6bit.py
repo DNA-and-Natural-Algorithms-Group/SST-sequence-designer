83cd423db4c4d3416c087c3f08baaf66ddeb2391
Damien Woods <woods@caltech.edu>
2018-11-03 23:27:22 +0000

    comments

--- input_tilesets/ibc_6bit.py
+++ input_tilesets/ibc_6bit.py
@@ -1,75 +1,66 @@
+
+# Input file that includes input tileset and threshold parameters for SST sequence designer. 
 #
 # Specification of 6-bit IBC tile set with 2x2 proofreading with energy thresholds 
-# as used in the below publication.
+# as used in the following publication.
 #
 # Woods*, Doty*, Myhrvold, Hui, Zhou, Yin, Winfree. (*Joint first co-authors)
 # Diverse and robust molecular algorithms using reprogrammable DNA self-assembly
 # 
+# Note that all energy parameter values are sign-flipped (i.e. more positive is more favourable)
 
 
 
-# "global" energy interval for all glues, except those glues with user-specified sequences
+# "global" energy interval for all glues, except those glues with user-specified sequences;
+# evaluated using a simple nearest-neighbour model
 lowDG=8.9
 highDG=9.2
 biotin_strength_boost=1.1
 
-# temperature at which to evaluate energies
+# temperature in degrees C at which to evaluate energies
 temperature = 53.0
     
-# allowed energy of secondary structure of individual tile
-tile_sec_struct = 1.65
+# allowed energy of secondary structure of each individual tile (via nupack)
+tile_sec_struct = 1.65 
 
-# allowed energy of secondary structure of pair of tiles that don't share any glues
+# allowed energy of secondary structure of each pair of tiles that don't share any glues (via RNAduplex)
 tile_orth = 5.4  
 
-# allowed energy of secondary structure of pair of tiles that share glues
-tile_orth_share = 7.4
+# allowed energy of secondary structure of pair of tiles that share glues (via RNAduplex)
+tile_orth_share = 7.4 
 
 # threshold for allowed energy of interaction between algorithmically conflicting glues
-# that might be co-located during a strength-1 binding event
+# that might be co-located during a strength-1 binding event on an otherwise perfect lattice (via nupack)
 orth_algorithmic_conflict = 1.6
 
+# threshold for allowed energy of interaction between algorithmically conflicting glues
+# that might be co-located during a strength-1 binding event on a lattice that possibly 
+# has some other kinds of error (via nupack)
 orth_algorithmic_conflict_generalized = 2.6
 
-# threshold for allowed energy of glues that will be colocated during correct growth (must be broken up to allow binding of new tile)
+# threshold for allowed energy of glues that will be colocated during correct growth 
+# (must be broken up to allow binding of new tile) (via nupack)
 orth_colocated = 1.4
 
-# tiles should bind to lattice with energy strictly greater than the following thresold
-# 11.5 gives 2 bad glues from recent seeds. 12.5 should be achievable (from looking at plot "growth on right (simplified binding(), lattice spacer is TTTTT).pdf").
+# tiles should bind to lattice with energy strictly greater than the following thresold (via nupack)
 lattice_binding_lower_threshold = 12.3
 
 
-
-# Below, negative values cause code to ignore the relevant parameter
-
-# minimum Hamming distance all domains must be from each other
-hamming = -1
-
-# threshold for allowed energy of interaction between orthogonal glues
-# that will not be co-located during a strength-1 binding event
-orth_any = -5.0
-
-# orth_any is worst-case; all pairs of sequences must be below
-# orth_any_ave is average orthogonality of proposed new domain with all
-# currently used domains
-orth_any_ave = -3.0
-
-# this is the limit on individual secondary structure of a single domain
-domain_indv_sec_struct = -0.3
-domain_indv_sec_struct_ave = -0.2
-
-# this is the limit on secondary structure of any two domains concatenated
-domain_pair_sec_struct = -20.0
-domain_pair_sec_struct_ave = -0.8
-
 # if this line is present, then parity 0 tiles will use a 3-letter code
-#  A,C,T, and parity 1 tiles will use A,G,T
+# A,C,T, and parity 1 tiles will use A,G,T
 three_letter_code = True
+
+# number of bases that are exceptions to the three letter code, e.g. TGACTGGTAAAG has 
+# three letter code A,T,G with a three_letter_code_exceptions = 1
 three_letter_code_exceptions = 1
 
+# if endGC == True then each domain begins and end with G or C, e.g. GTTAAGCGTCC 
 endGC = False
+# if endAT == True then each domain begins and end with A or T, e.g. TTTAAGCGTCA
 endAT = True
 
+# when designing deterministic tilesets setting detect_nondeterminism=True performs 
+# a syntax check on the tile set to ensure there is no nondeterminism 
 detect_nondeterminism=False
 
 
@@ -564,7 +555,33 @@ glue_sequences = [
 
 import itertools as it
 
-# If this is defined, it defines mutually exclusive pairs of tiles (their names, actually) that will never be together in the same pot 
+
+
+# The following variables make use of features in the sequence designer that not exploited 
+# when designing DNA sequence for the 6-bit IBC tile set. In particular, assigning negative values 
+# acts as a switch to cause the sequence design code to ignore the relevant parameter.
+
+# minimum Hamming distance all domains must be from each other
+hamming = -1
+
+# threshold for allowed energy of interaction between orthogonal glues
+# that will not be co-located during a strength-1 binding event
+orth_any = -5.0
+
+# orth_any is worst-case; all pairs of sequences must be below
+# orth_any_ave is average orthogonality of proposed new domain with all
+# currently used domains
+orth_any_ave = -3.0
+
+# this is the limit on individual secondary structure of a single domain
+domain_indv_sec_struct = -0.3
+domain_indv_sec_struct_ave = -0.2
+
+# this is the limit on secondary structure of any two domains concatenated
+domain_pair_sec_struct = -20.0
+domain_pair_sec_struct_ave = -0.8
+
+# If the following list is defined, it defines mutually exclusive pairs of tiles (their names, actually) that will never be together in the same pot 
 # The program will not bother comparing any glues/tiles to each other if they appear as a pair here
 # The code below finds pairs of tiles with the same (non-full) name (e.g., U6), 
 #   and the same input to the function they compute, but different output
