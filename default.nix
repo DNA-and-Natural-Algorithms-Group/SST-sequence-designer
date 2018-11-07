68983e3eff22b3f8c76b4d86df8f367751dc5b2d
Damien Woods <woods@caltech.edu>
2018-11-07 23:37:26 +0000

    NUPACKHOME bug

--- default.nix
+++ default.nix
@@ -1,7 +1,5 @@
 # Nix script to set up appropriate environmen to run python2-based SST sequence designer.
 #
-# 
-# 
 
 with import <nixpkgs>{};  # import nixos packages
 
@@ -15,6 +13,7 @@ let
     installPhase=''  # installPhase intreprets double-quoted string as a bash script
       mkdir -p $out; # $out is bash variable that nix sets before running this bash script
       cp -r bin $out;
+      cp -r parameters $out;
      '';
   }; 
 
@@ -26,35 +25,17 @@ let
 
   viennaRNA=stdenv.mkDerivation{
     name="viennaRNA";
-    #preConfigure="export CC=${pkgs.gcc.out}/bin/gcc";
-    #preBuild="export CC=${pkgs.gcc.out}/bin/gcc";
     src=./nupack_viennaRNA/ViennaRNA-2.1.9.tar.gz;
     buildInputs=[which perl gnumake];
-    buildPhase=''    # writing a multiline string using 2 pairs of single quotes, that buildPHase intreprets as a bash script
-      echo "Running the viennaRNA build phase" 
-      #./configure
-      #make
-      #make check
-      '';
-
     installPhase='' 
     make install
-    #make clean
-    # The following bash commands copy the compiled binaries to the output.
-    # We found (by running "find ." here) that the binaries are compiled in the 
-    # build stage and placed in the directory ./src/bin . Different systems provide
-    # different versions of ls, so we include coreutils below to get whatever is 
-    # the ls shipped with nix coreutils 
-    #mkdir -p $out/bin 
-    #for i in $(ls src/bin/RNA* | grep -v "\." ); do
-    #  cp $i $out/bin
-    #done 
    	'';
   }; in  
 
 stdenv.mkDerivation{
   name="nupack_viennaRNA";
-  buildInputs=[coreutils python2 python2Packages.numpy nupack viennaRNA];   # here I put a list of names of packages I want to install
+  buildInputs=[coreutils python2 python2Packages.numpy nupack viennaRNA]; #   python2Packages.matplotlib python3Packages.matplotlib  python3 python3Packages.numpy   # here I put a list of names of packages I want to install
+  NUPACKHOME="${nupack.out}";
 }
 
 
